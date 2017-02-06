dfdaec1e4aeb892902fde859bb4ca6f8a024eccb
orangeFK <BLueLotus@zhongdeMacBook-Pro.local>
2017-02-06 10:17:16 +0800

    test

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubTest
+//
+//  Created by BLueLotus on 2017/2/6.
+//  Copyright © 2017年 BLueLotus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
