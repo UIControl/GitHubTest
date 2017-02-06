d252f0d523a218fda19006ecb14cda1e61ce8cf1
orangeFK <BLueLotus@zhongdeMacBook-Pro.local>
2017-02-06 10:20:07 +0800

    fk

---
+++ FKViewController.h
@@ -0,0 +1,13 @@
+//
+//  FKViewController.h
+//  GitHubTest
+//
+//  Created by BLueLotus on 2017/2/6.
+//  Copyright © 2017年 BLueLotus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FKViewController : UIViewController
+
+@end
---
+++ FKViewController.m
@@ -0,0 +1,37 @@
+//
+//  FKViewController.m
+//  GitHubTest
+//
+//  Created by BLueLotus on 2017/2/6.
+//  Copyright © 2017年 BLueLotus. All rights reserved.
+//
+
+#import "FKViewController.h"
+
+@interface FKViewController ()
+
+@end
+
+@implementation FKViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
